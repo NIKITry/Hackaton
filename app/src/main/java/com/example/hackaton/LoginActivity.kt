8846bbb5e362a183556c0a6ab6225c40d10350b8
NIKITry <nikitabandurin2002@gmail.com>
2023-04-01 19:16:12 +0300

    add jump from LoginActivity to MainActivity

--- app/src/main/java/com/example/hackaton/LoginActivity.kt
+++ app/src/main/java/com/example/hackaton/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hackaton
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hackaton.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
