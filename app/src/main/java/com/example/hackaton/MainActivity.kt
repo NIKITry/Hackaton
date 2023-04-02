31042fd0cce2255d4e2720577d3afa34b30cae7b
NIKITry <nikitabandurin2002@gmail.com>
2023-04-02 04:41:20 +0300

    little fixes

--- app/src/main/java/com/example/hackaton/MainActivity.kt
+++ app/src/main/java/com/example/hackaton/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(MaterialFragment())
+        replaceFragment(ProfileFragment())
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.game -> {
